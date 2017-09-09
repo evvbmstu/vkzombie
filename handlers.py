9d401e747131e9b6a8555dfb494254ed14265c18
SanyCska <alexander2302@mail.ru>
2017-09-09 22:04:44 +0300

    fixed bug with reversing words

--- handlers.py
+++ handlers.py
@@ -90,7 +90,10 @@ def formatter( parts ):
         except IndexError:
                 return "Команда введена не полностью, правильный вариант можно посмотреть по запросу <команды>"
         if '-' not in parts[1]:
-            digits = re.search(r'\d+', parts[1]).group()
+            try:
+                digits = re.search(r'\d+', parts[1]).group()
+            except AttributeError:
+                return "Неправильно введена команда. Возможно ты перепутал местами <группу> и <форму обучения> в запросе. Попробуй еще раз!"
             index = unicode(re.search(r'\D+',parts[1]).group(),'utf-8').upper()
         else:
             group  = unicode( parts[1], "utf-8" ).upper()
